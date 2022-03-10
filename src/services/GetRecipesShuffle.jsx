963f48899aae27e251915ca8bb6e595f07c13099
Hugo Cortez <hugo.cortez@t4i.intranet>
2022-03-10 02:49:30 -0300

    adicionando ordenação alfabetica nas receitas

--- src/services/GetRecipesShuffle.jsx
+++ src/services/GetRecipesShuffle.jsx
@@ -11,8 +11,20 @@ function shuffle(arr){
     return arr
 }
 
+function compare_lname( a, b )
+    {
+    if ( a.name.toLowerCase() < b.name.toLowerCase()){
+        return -1;
+    }
+    if ( a.name.toLowerCase() > b.name.toLowerCase()){
+        return 1;
+    }
+    return 0;
+}
+
 export function getRecipesShuffle(recipeName){
     const recipes = loadRecipes()
-    var result = shuffle(recipes)
+    var result = recipes.sort(compare_lname);
+    //var result = shuffle(recipes)
     return result
 }
